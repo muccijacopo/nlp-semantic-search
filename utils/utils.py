052099b52437c9f2c7f180a76a1477a27b108bac
muccijacopo <mucci.jacopo@gmail.com>
2022-11-08 22:49:56 +0100

    Project init; xml reading and csv export; data analysis and preprocessing

---
+++ utils/utils.py
@@ -0,0 +1,13 @@
+import re
+from typing import Dict
+
+ATTRIBUTES = ['Id', 'PostTypeId', 'CreationDate', 'Score', 'ViewCount', 'FavoriteCount', 'Title', 'Body', 'Tags']
+
+def normalize_string(s: str) -> str:
+    return re.sub('(<.{1,5}>)|\n', "", s)
+
+def get_post_attrib(post: Dict[str, str]) -> Dict[str, str]:
+    r = {}
+    for a in ATTRIBUTES:
+        r[a] = post.get(a) or ""
+    return r
