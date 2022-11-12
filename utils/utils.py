15939999c219a300e7bad8574e6b3796464360af
muccijacopo <mucci.jacopo@gmail.com>
2022-11-12 19:10:07 +0100

    feat: Word2Vec model; preprocessing reworking

--- utils/utils.py
+++ utils/utils.py
@@ -3,8 +3,13 @@ from typing import Dict
 
 ATTRIBUTES = ['Id', 'PostTypeId', 'CreationDate', 'Score', 'ViewCount', 'FavoriteCount', 'Title', 'Body', 'Tags']
 
+# def normalize_string(s: str) -> str:
+#     return re.sub('(<.{1,5}>)|\n', "", s)
+
+
 def normalize_string(s: str) -> str:
-    return re.sub('(<.{1,5}>)|\n', "", s)
+    return re.sub('\n', "", s)
+
 
 def get_post_attrib(post: Dict[str, str]) -> Dict[str, str]:
     r = {}
