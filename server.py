39e1788c262acbf7b20a46c6a9819fea5e8b45e5
muccijacopo <mucci.jacopo@gmail.com>
2023-02-19 21:56:08 +0100

    feat: add response time to query response

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
-import json
-
+import time
 from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse, JSONResponse
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from main import train_model
 
+from main import train_model
 from query import Query
 
 app = FastAPI(title="Semantic Search API")
@@ -14,8 +13,13 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
 @app.get("/query")
 def read_root(q: str, topic: str, model: str, format: str):
+    start_time = time.time()
     r = Query.make_query(q, topic=topic, model=model, generate_text=format == 'answer')
-    return JSONResponse(r)
+    end_time = time.time()
+    return JSONResponse({
+        'documents': r,
+        'response_time': end_time - start_time
+    })
 
 
 @app.get("/train")
