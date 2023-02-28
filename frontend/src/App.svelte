aec969a0e6b736e01ce2f57cf05da0eace85ffb9
muccijacopo <mucci.jacopo@gmail.com>
2023-02-28 18:19:53 +0100

    style: app restlying

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,8 +3,8 @@
   import type { Document, Response } from "./interfaces/results";
   import Loader from "./lib/Loader.svelte";
 
-  let searchText = "What is the difference between IPA and Double IPA?";
-  let topic = "beer";
+  let searchText = "How can I replace missing values in a Pandas dataframe?";
+  let topic = "datascience";
   let model = "doc2vec";
   let documents: Document[] = [];
   let responseTime: number;
@@ -41,30 +41,45 @@
   }
 </script>
 
-<h1>NLP Semantic Search</h1>
-<div class="search-wrapper">
-  <div class="search-input">
-    <input type="text" bind:value={searchText} />
-    <button on:click={query} disabled={!searchText.length}>SEARCH</button>
+<div class="container">
+  <div class="header">
+    <h1>This is a semantic search engine.</h1>
+    <p>
+      The project aims to recreate a semantic search engine using Natural
+      Language Processing and Deep Learning techniques. The search experience
+      can be customized using a different language model. Search results will
+      depend on the model used and the data set. This project is a work related
+      to a Computer Engineering Master Thesis at Roma Tre University: "Natural
+      Language Processing and Deep Learning Techniques for a Semantic Search
+      Engine". <br />
+      Source code can be found on
+      <a href="https://github.com/muccijacopo">Github</a>
+    </p>
   </div>
-  <div style="display: flex; justify-content: center; gap: 10px">
-    <h3>Model</h3>
-    <select bind:value={model}>
-      {#each models as model}
-        <option value={model.key}>{model.name}</option>
-      {/each}
-    </select>
-    <h3>Dataset</h3>
-    <select bind:value={topic}>
-      {#each topics as topic}
-        <option value={topic.key}>{topic.name}</option>
-      {/each}
-    </select>
-    <h3>Format</h3>
-    <select>
-      <option>Documents</option>
-      <option disabled>Answer</option>
-    </select>
+  <div class="search-wrapper">
+    <div class="search-input">
+      <input type="text" bind:value={searchText} />
+      <button on:click={query} disabled={!searchText.length}>SEARCH</button>
+    </div>
+    <div style="display: flex; justify-content: center; gap: 10px">
+      <h3>Model</h3>
+      <select bind:value={model}>
+        {#each models as model}
+          <option value={model.key}>{model.name}</option>
+        {/each}
+      </select>
+      <h3>Dataset</h3>
+      <select bind:value={topic}>
+        {#each topics as topic}
+          <option value={topic.key}>{topic.name}</option>
+        {/each}
+      </select>
+      <h3>Format</h3>
+      <select>
+        <option>Documents</option>
+        <option disabled>Answer</option>
+      </select>
+    </div>
   </div>
 </div>
 
@@ -93,8 +108,15 @@
 {/if}
 
 <style>
-  h1 {
-    font-size: 60px;
+  .container {
+    width: 100%;
+    max-width: 900px;
+    margin: 0 auto;
+    text-align: left;
+  }
+  .header {
+    /* max-width: 600px; */
+    margin-bottom: 50px;
   }
   .search-wrapper {
     display: flex;
@@ -110,6 +132,7 @@
   }
   .search-input {
     display: flex;
+    justify-content: center;
     gap: 10px;
   }
   select {
@@ -129,7 +152,7 @@
     overflow-y: auto;
   }
   .card {
-    background-color: var(--content-bg);
+    background-color: var(--secondary-color);
     border-radius: var(--border-radius);
     padding: 10px 20px;
     text-align: left;
