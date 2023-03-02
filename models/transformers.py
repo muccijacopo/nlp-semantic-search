1187e67e796cbc9dc296648d6019fc3cc41c148c
muccijacopo <mucci.jacopo@gmail.com>
2023-03-02 21:05:46 +0100

    feat: updated results

--- models/transformers.py
+++ models/transformers.py
@@ -5,7 +5,7 @@ from sentence_transformers import SentenceTransformer, util, models, InputExampl
 import torch
 from torch.utils.data import DataLoader, Dataset
 from transformers import AutoTokenizer, AutoModel, GPT2Tokenizer, GPT2Model, pipeline, set_seed, \
-    QuestionAnsweringPipeline, OpenAIGPTModel
+    QuestionAnsweringPipeline, OpenAIGPTModel, berttoke
 
 from models import Model
 from corpus import Corpus
